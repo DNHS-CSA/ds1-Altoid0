70889ca2de4531e5601b0feef68eab45bc101271
Tanay Shah <tanay.v.shah@gmail.com>
2020-03-10 10:44:18 -0700

    Initial Commit

---
+++ SwiftPortfolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPortfolio
+//
+//  Created by Shah, Tanay on 3/10/20.
+//  Copyright © 2020 Shah, Tanay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
